287815e648f92950a3f1c3dcc4c68ffcdfc047d0
RamonSanGabriel <jeshero11@gmail.com>
2024-10-01 23:23:11 +0800

    update

--- models/contacts.js
+++ models/contacts.js
@@ -1,21 +1,73 @@
-// const fs = require('fs/promises')
+import fs from 'fs/promises';
 
-const listContacts = async () => {};
+import path from 'path';
+import { nanoid } from 'nanoid';
 
-const getContactById = async (contactId) => {};
+export const contactsPath = path.join('models', 'contacts.json');
 
-const removeContact = async (contactId) => {};
+const listContacts = async () => {
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  } catch (error) {
+    console.error('Error reading contacts:', error.message);
+  }
+};
 
-const addContact = async (body) => {};
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((contact) => contact.id === contactId);
+    return result || null;
+  } catch (error) {
+    console.error('Error reading the contacts by id!', error.message);
+  }
+};
 
-const updateContact = async (contactId, body) => {};
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((item) => item.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const deleteContact = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return deleteContact;
+  } catch (error) {
+    console.error('Error removing contact:', error.message);
+  }
+};
 
-/* module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-} */
-// prettier-ignore
-export {listContacts, getContactById, removeContact, addContact, updateContact};
+const addContact = async ({ name, email, phone }) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: nanoid(), name, email, phone };
+    const allContacts = [...contacts, newContact];
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error('Error adding new contact:', error.message);
+  }
+};
+
+const updateContact = async (id, { name, email, phone }) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === id);
+
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = {
+    id,
+    name,
+    email,
+    phone,
+  };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
+
+//prettier-ignore
+export { listContacts, getContactById, removeContact, addContact, updateContact };
